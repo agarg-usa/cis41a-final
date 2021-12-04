f2a0802824e5a5af4fe37f2eca6ac270aa1670ac
agarg.usa <agarg.usa@gmail.com>
2021-12-04 11:50:09 -0800

    added to burger

--- burger.py
+++ burger.py
@@ -3,18 +3,37 @@ class Burger:
 		self.name = name
 		self.price = price
 		self.description = description
+
 	def print_description(self):
 		raise NotImplementedError("Subclass must implement abstract method")
 
 class BurgerCollection:
 	def __init__(self, burgers):
 		self.burgers = burgers
+		self.burgerDict = {}
+		for burger in burgers:
+			self._add_to_dict(burger)
 
-	def show_menu():
-		print("Welcome to De Anza Grill!")
-		print("1 - De Anza Burger - $5.25")
-		print("2 - Bacon Cheese - $5.75")
-		print("3 - Mushroom Swiss - $5.95")
-		print("4 - Western Burger - $5.95")
-		print("5 - Don Cali Burger - $5.95")
-		print("6 - Exit")
+	def add_burger(self, burger):
+		self.burgers.append(burger)
+
+	def _add_to_dict(self, burger):
+		self.burgerDict[burger.name] = burger
+
+	def show_menu(self):
+		for burger in self.burgers:
+			burger.print_description()
+
+	def get_burger_by_name(self, name):
+		return self.burgerDict[name]
+
+	def get_burgers(self):
+		return self.burgers
+
+class DeAnzaBurger(Burger):
+	def __init__(self, name, price, description):
+		super().__init__(name, price, description)
+
+	def print_description(self):
+		print(f"{self.price}: {self.name} + {self.description}")
+
